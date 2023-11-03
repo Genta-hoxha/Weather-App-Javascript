8903de6de1e4560eff2876e7372635f4f14bbbd2
Genta-hoxha <genta.hoxha10@gmail.com>
2023-11-03 16:27:30 +0100

    weather app

--- js/script.js
+++ js/script.js
@@ -18,12 +18,20 @@ function checkWeather(cityname) {
     .then((res) => {
       obj = res;
       if (!res.ok) {
-        throw new Error("Unable to fetch weather data.");
+        // throw new Error("Unable to fetch weather data.");
+        throw alert("Qyteti nuk u gjend!");
       }
       return res.json();
     })
     .then((data) => {
       const tempCelsius = Math.round(data.main.temp);
+      const tempMin = data.main.temp_min;
+      const tempMax = data.main.temp_max;
+
+      // const tempCelsius = Math.round(data.main.temp);
+      // const tempMin = data.main.temp_min;
+      // const tempMax = data.main.temp_max;
+
       const html = `
         <div id="weather">
           <h2 class="city">${data.name}, ${data.sys.country}</h2>
@@ -31,13 +39,18 @@ function checkWeather(cityname) {
           <h1 class="temp">${tempCelsius} °C</h1>
           <h3 class="description">${data.weather[0].description}</h3>
           <div class="degree-button">
-            <h3 class="tempMin">Temp.min: ${data.main.temp_min} <button id="celsiusButton" onclick="convertToCelsius()">°C</button> /
+            <h3 class="tempMin">Temp.min: ${tempMin} <button id="celsiusButton" onclick="convertToCelsius()">°C</button> /
             <button id="fahrenheitButton" onclick="convertToFahrenheit()">°F</button> </h3>
-                   <h3 class="tempMax">Temp.max: ${data.main.temp_max} <button id="celsiusButton" onclick="convertToCelsius()">°C</button> /
+                   <h3 class="tempMax">Temp.max: ${tempMax} <button id="celsiusButton" onclick="convertToCelsius()">°C</button> /
             <button id="fahrenheitButton" onclick="convertToFahrenheit()">°F</button>
             </h3>
           </div>
-        
+          <center>
+          <div class="temperature-strip">
+          <div class="strip"></div>
+      </div>
+      </center>
+
           <div class="details">
             <div class="col">
               <i class="fa-solid fa-water fa-2x"></i>
@@ -55,11 +68,26 @@ function checkWeather(cityname) {
             </div>
           </div>
         </div>
-        
-      `;
-
+              `;
       const weather = document.getElementById("weather");
       weather.innerHTML = html;
+
+      const range = tempMax - tempMin;
+      const position = tempCelsius - tempMin;
+      const percentage = (position / range) * 100;
+
+      const temperatureStrip = document.querySelector(".strip");
+
+      temperatureStrip.style.width = `${percentage}%`;
+
+      if (tempCelsius < tempMin) {
+        temperatureStrip.style.backgroundColor = "blue";
+      } else if (tempCelsius > tempMax) {
+        temperatureStrip.style.backgroundColor = "red";
+      } else {
+        temperatureStrip.style.backgroundColor = "green";
+      }
+
       cel = tempCelsius;
     })
     .catch((error) => {
@@ -93,11 +121,18 @@ function convertToFahrenheit() {
 
 let saveData = [];
 addButton.addEventListener("click", () => {
-  console.log("hello");
+  // console.log("hello");
   saveData.push(searchBox.value);
   let span = document.createElement("span");
   span.className = "dot";
   slideshow.appendChild(span);
+
+  if (saveData.length == 1) {
+    let dots = slideshow.querySelectorAll(".dot");
+    dots.forEach((dot, i) => {
+      dot.classList.toggle("active");
+    });
+  }
 });
 
 document.addEventListener("click", (e) => {
@@ -105,11 +140,13 @@ document.addEventListener("click", (e) => {
     console.log(saveData, e.target);
     e.stopPropagation();
     checkWeather(saveData[Array.from(slideshow.children).indexOf(e.target)]);
+
     let cityName =
       saveData[Array.prototype.indexOf.call(slideshow.children, e.target)];
 
     const index = saveData.indexOf(cityName);
     console.log(index);
+
     let dots = slideshow.querySelectorAll(".dot");
     dots.forEach((dot, i) => {
       if (i === index) {
